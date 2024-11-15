a900f8cf59732122b14e73deb18f543a0615479b
henrideanton <henriqdeantoni@gmail.com>
2024-11-15 12:00:22 -0300

    feat: primeiro commit

---
+++ CursoWindowsFormsBiblioteca/Databases/FicharioDB.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CursoWindowsFormsBiblioteca.Classes.Cliente;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class FicharioDB
+    {
+
+        public string mensagem;
+        public bool status;
+        public string tabela;
+        public LocalDBClass db;
+
+        public FicharioDB(string Tabela)
+        {
+            status = true;
+
+            try
+            {
+                db = new LocalDBClass();
+                tabela = Tabela;
+                mensagem = "Conexão com a tabela criada com sucesso.";
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com a tabela com erro: " + ex.Message;
+            }
+        }
+
+        public void Incluir(string Id, string jsonUnit)
+        {
+            status = true;
+            try
+            {
+                //INSERT INTO CLIENTE (ID, JSON) VALUES (`000001`, `{...}')
+
+                var SQL = "INSERT INTO " + tabela + "(Id, JSON) VALUES ('" + Id + "', '" + jsonUnit + "')";
+                db.SQLCommand(SQL);
+
+                status = false;
+                mensagem = "Inclusão não permitida porque o identificador já existe: " + Id;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+            }
+        }
+
+        public string Buscar(string Id)
+        {
+            status = true;
+            try
+            {
+                // SELECT ID, JSON FROM CLIENTE WHERE ID = '000001'
+
+                var SQL = "SELECT Id, JSON FROM " + tabela + " WHERE ID = '" + Id + "'";
+                var dt = db.SQLQuery(SQL);
+
+
+                if(dt.Rows.Count >0)
+                {
+                    string conteudo = dt.Rows[0]["JSON"].ToString();
+                    status = true;
+                    mensagem = "Inclusão efetuada com sucesso. Identificador: " + Id;
+                    return conteudo;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existente: " + Id;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+            return "";
+        }
+
+        public List<string> BuscarTodos()
+        {
+            status = true;
+            List<string> List = new List<string>();
+            try
+            {
+                // SELECT ID, JSON FROM CLIENTE
+                var SQL = "SELECT Id, JSON FROM " + tabela;
+                var dt = db.SQLQuery(SQL);
+
+                if(dt.Rows.Count > 0)
+                {
+                    for(int i = 0; i < dt.Rows.Count-1; i++)
+                    {
+                        string conteudo = dt.Rows[i]["json"].ToString();
+                        List.Add(conteudo);
+                    }
+                    return List;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+            return List;
+        }
+
+        public void Apagar(string Id)
+        {
+            status = true;
+            try
+            {
+                
+                var SQL = "SELECT Id, JSON FROM " + tabela;
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0)
+                {
+                    //DELETE FROM CLIENTE WHERE Id = `000010`
+                    SQL = "DELETE FROM " + tabela + " WHERE ID = '" + Id + "'";
+                    db.SQLCommand(SQL);
+                    status = true;
+                    mensagem = "Inclusão efetuada com sucesso. Identificador: " + Id;
+                    
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existente: " + Id;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+        }
+
+        public void Alterar(string Id, string jsonUnit)
+        {
+            status = true;
+            try
+            {
+                
+                var SQL = "SELECT Id, JSON FROM " + tabela;
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0)
+                {
+                    //UPDATE CLIENTE SET JSON = '{...}' WHERE ID = '000010'
+                    SQL = "UPDATE " + tabela + " SET JSON = '" + jsonUnit + "' WHERE ID = '" + Id + "'";
+                    db.SQLCommand(SQL);
+                    status = true;
+                    mensagem = "Alteração efetuada com sucesso. Identificador: " + Id;
+
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existente: " + Id;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+            }
+
+        }
+    }
+}
---
+++ CursoWindowsFormsBiblioteca/Databases/LocalDBClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class LocalDBClass
+    {
+        public string stringConn;
+        public SqlConnection connDB;
+
+        public LocalDBClass()
+        {
+
+            try
+            {
+                stringConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\sup-not-26\\OneDrive\\# WORKING\\Alura\\modulo_6\\segunda_parte\\CursoWindowsFormsBiblioteca\\Databases\\Fichario.mdf\";Integrated Security=True";
+
+                connDB = new SqlConnection(stringConn);
+                connDB.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        public DataTable SQLQuery(string SQL)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                var myCommand = new SqlCommand(SQL, connDB);
+                myCommand.CommandTimeout = 0; //=0 significa que espera o tempo que for necessario, não existe time out
+                var myReader = myCommand.ExecuteReader();
+                dt.Load(myReader);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return dt;
+        }
+
+        public string SQLCommand(string SQL)
+        {
+            try
+            {
+                var myCommand = new SqlCommand(SQL, connDB);
+                myCommand.CommandTimeout = 0; //=0 significa que espera o tempo que for necessario, não existe time out
+                var myReader = myCommand.ExecuteReader();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Close()
+        {
+            connDB.Close();
+        }
+    }
+}
