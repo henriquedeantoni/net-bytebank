e74494b6800ff43bd45f3fc7683a8deb9004b98f
henriquedeantoni <henriqdeantoni@gmail.com>
2024-11-28 01:18:00 -0300

    feat: adicionado metodo toupdate

--- CursoWindowsFormsBiblioteca/Classes/Cliente.cs
+++ CursoWindowsFormsBiblioteca/Classes/Cliente.cs
@@ -467,7 +467,35 @@ namespace CursoWindowsFormsBiblioteca.Classes
                 SQL += "," + Convert.ToString(this.RendaFamiliar) + ");";
 
                 return SQL;
-                }
+            }
+
+            public string ToUpdate(string Id)
+            {
+                string SQL;
+
+                SQL = @"UPDATE TB_Cliente
+                        SET";
+                SQL += "Id = '" + this.Id + "'";
+
+                SQL += ", Nome = '" + this.Nome + "'";
+                SQL += ", NomePai = '" + this.NomePai + "'";
+                SQL += ", NomeMae = '" + this.NomeMae + "'";
+                SQL += ", NaoTemPai = " + Convert.ToString(this.NaoTemPai) + "";
+                SQL += ", Cpf = '" + this.Cpf + "'";
+                SQL += ", Genero = " + Convert.ToString(this.Genero) + "";
+                SQL += ", Cep = '" + this.Cep + "'";
+                SQL += ", Logradouro = '" + this.Logradouro + "'";
+                SQL += ", Complemento = '" + this.Complemento + "'";
+                SQL += ", Bairro = '" + this.Bairro + "'";
+                SQL += ", Cidade = '" + this.Cidade + "'";
+                SQL += ", Estado = '" + this.Estado + "'";
+                SQL += ", Telefone = '" + this.Telefone + "'";
+                SQL += ", Profissao = '" + this.Profissao + "'";
+                SQL += ", RendaFamiliar = " + Convert.ToString(this.RendaFamiliar) + "";
+                SQL += "WHERE Id = '"+ Id +"';";
+
+                return SQL;
+            }
 
 
             #endregion
